7aebeb2392dcd907ec42032bef6bce466ab15667
Ankush <91677627+Noveleader@users.noreply.github.com>
2023-04-11 20:48:12 +0530

    Update FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -1,38 +1,39 @@
-#include<stdio.h> 
-#include<conio.h> 
-void main(){ 
-  int bt[10]={0},at[10]={0},tat[10]={0},wt[10]={0},ct[10]={0}; 
-  int n,sum=0; float totalTAT=0,totalWT=0; 
-  printf("Enter number of processes: "); 
-  scanf("%d",&n); 
-  printf("Enter Arrival time and Burst time for each process:\n"); 
-  for(int i=0;i<n;i++) 
-  { 
-    printf("Arrival time of process[%d]: ",i+1); 
-    scanf("%d",&at[i]); 
-    printf("Burst time of process[%d]: ",i+1); 
-    scanf("%d",&bt[i]); 
-  } 
-  for(int j=0;j<n;j++) 
-  { 
-      sum+=bt[j]; 
-      ct[j]+=sum; 
-  } 
-  for(int k=0;k<n;k++) 
-  { 
-      tat[k]=ct[k]-at[k]; 
-      totalTAT+=tat[k]; 
-  } 
-  for(int k=0;k<n;k++) 
-  { 
-      wt[k]=tat[k]-bt[k]; 
-      totalWT+=wt[k]; 
-  } 
-  printf("P#\t AT\t BT\t CT\t TAT\t WT\t\n\n"); 
-  for(int i=0;i<n;i++) 
-  { 
-    printf("P%d\t %d\t %d\t %d\t %d\t %d\n",i+1,at[i],bt[i],ct[i],tat[i],wt[i]); 
-  } 
-  printf("\nAverage Turnaround Time = %f\n",totalTAT/n); 
-  printf("Average WT = %f\n\n",totalWT/n); getch(); 
+#include <stdio.h>
+int waitingtime(int proc[], int n,
+int burst_time[], int wait_time[]) {
+   wait_time[0] = 0;
+   for (int i = 1; i < n ; i++ )
+   wait_time[i] = burst_time[i-1] + wait_time[i-1] ;
+   return 0;
+}
+int turnaroundtime( int proc[], int n,
+int burst_time[], int wait_time[], int tat[])
+ {
+   int i;
+   for ( i = 0; i < n ; i++)
+   tat[i] = burst_time[i] + wait_time[i];
+   return 0;
+}
+int avgtime( int proc[], int n, int burst_time[])
+ {
+   int wait_time[n], tat[n], total_wt = 0, total_tat = 0;
+   int i;
+   waitingtime(proc, n, burst_time, wait_time);
+   turnaroundtime(proc, n, burst_time, wait_time, tat);
+   printf("Processes  Burst   Waiting Turn around \n");
+   for ( i=0; i<n; i++) {
+  	total_wt = total_wt + wait_time[i];
+  	total_tat = total_tat + tat[i];
+  	printf(" %d\t  %d\t\t %d \t%d\n", i+1, burst_time[i], wait_time[i], tat[i]);
+   }
+   printf("Average waiting time = %f\n", (float)total_wt / (float)n);
+   printf("Average turn around time = %f\n", (float)total_tat / (float)n);
+   return 0;
+}
+int main() {
+   int proc[] = { 1, 2, 3};
+   int n = sizeof proc / sizeof proc[0];
+   int burst_time[] = {5, 8, 12};
+   avgtime(proc, n, burst_time);
+   return 0;
 }
